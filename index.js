495330a0d9afcd48695187b71bb0dda6202dee90
cang <ho.kimcang@gmail.com>
2022-12-01 15:20:29 -0500

    change PORT variable

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
 
-server.listen(8000, () => {
+server.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
 });
