82a0ec5b8e9ace23c713f3ea7ababe37fd74aab1
cang <ho.kimcang@gmail.com>
2022-12-01 15:27:12 -0500

    add home route handler

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
