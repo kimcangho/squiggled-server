b6f17d47682812c16b7f8ed2f16dbcffd35bf70a
cang <ho.kimcang@gmail.com>
2023-01-02 23:16:52 -0500

    add leave room handler and add participant maximum when joining room

--- roomHandler/roomHandler.js
+++ roomHandler/roomHandler.js
@@ -1,18 +1,55 @@
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidV4 } = require("uuid");
 
+//Store existing rooms server-side
+//To-do: store in database
+const roomsArr = {};
+
+//Handle create/join/leave room events
 const roomHandler = (socket) => {
+
   const createRoom = () => {
-    const roomId = uuidv4();
-    socket.join(roomId);
-    socket.emit("room-created", { roomId });
+    const roomId = uuidV4(); //Generate random id
+    roomsArr[roomId] = []; //Create room key with empty array value
+    socket.join(roomId); //User joins room
+    socket.emit("room-created", { roomId }); //Emit to client
     console.log("user has created room!");
   };
-  const joinRoom = ({ roomId }) => {
-    console.log(`user has joined room ${roomId}`);
+
+  const joinRoom = ({ roomId, peerId }) => {
+    //Handle room larger than 2
+    if (roomsArr[roomId].length > 2) {
+      console.log("cruckets");
+      socket.emit("room-full", roomId);
+    }
+
+    //Check if room exists
+    if (roomsArr[roomId]) {
+      console.log(`user ${peerId} has joined room ${roomId}`);
+      roomsArr[roomId].push(peerId); //Add peer to room
+      socket.join(roomId); //User joins room
+         //Send back roomId and its array of participants
+      socket.emit("get-users", {
+        roomId,
+        participants: roomsArr[roomId],
+      });
+    }
+
+    socket.on("disconnect", () => {
+      console.log(`user ${peerId} has left the room`);
+      leaveRoom({ roomId, peerId });
+    });
   };
+
+  const leaveRoom = ({ roomId, peerId }) => {
+    roomsArr[roomId] = roomsArr[roomId].filter((id) => id !== peerId);  //Filter out peer that leaves
+    socket.to(roomId).emit("user-disconnected", peerId);    //emit to all others in room
+  };
+
   //Socket Room Listeners
   socket.on("create-room", createRoom);
   socket.on("join-room", joinRoom);
+  socket.on('leave-room', leaveRoom);
+
 };
 
 module.exports = roomHandler;
